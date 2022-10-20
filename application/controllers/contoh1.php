09374940067ff6f84d259efea7ffb1162943c456
Bambang Permadi <bambangpermadi46@gmail.com>
2022-10-20 20:43:23 +0700

    add comment

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,6 +3,7 @@ class Contoh1 extends CI_Controller
 {
 	public function index()
 	{
+			//trial comment
 		echo "<h1>Perkenalkan</h1>";
 		echo "Nama saya Imam Nawawi
  Saya tingga di daerah Ciputat
